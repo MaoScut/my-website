3aacb0c21145e8655fd7f328d83e133deb1cab85
MaoScut <849345372@qq.com>
2017-07-18 22:02:01 +0800

    不知怎么的，再连接就没有错误了，它直接帮我建立好数据库？感觉不太好实现呢 db方法的返回值，是不能用的，都是异步的，想处理结果，要传入回调函数 比如find.toArray((err,docs)=>....) 又或者 stats()和insert()都是返回一个promise

--- db.js
+++ db.js
@@ -6,4 +6,27 @@ MongoClient.connect(uri, (err, db) => {
 		console.log(err);
 		return;
 	}
+db.collection('inventory').find({ status: "D" }).toArray((err, docs) => {
+	console.log(docs);
+});
+// db.collection('inventory').stats().then(result => {
+// 	console.log(result);
+// });
+// db.collection('inventory').insertMany([
+//    // MongoDB adds the _id field with an ObjectId if _id is not present
+//    { item: "journal", qty: 25, status: "A",
+//        size: { h: 14, w: 21, uom: "cm" }, tags: [ "blank", "red" ] },
+//    { item: "notebook", qty: 50, status: "A",
+//        size: { h: 8.5, w: 11, uom: "in" }, tags: [ "red", "blank" ] },
+//    { item: "paper", qty: 100, status: "D",
+//        size: { h: 8.5, w: 11, uom: "in" }, tags: [ "red", "blank", "plain" ] },
+//    { item: "planner", qty: 75, status: "D",
+//        size: { h: 22.85, w: 30, uom: "cm" }, tags: [ "blank", "red" ] },
+//    { item: "postcard", qty: 45, status: "A",
+//        size: { h: 10, w: 15.25, uom: "cm" }, tags: [ "blue" ] }
+// ])
+// .then(function(result) {
+//   // process result
+//   console.log(result);
+// })
 })
